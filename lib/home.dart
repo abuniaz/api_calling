f7b3dd2419a03d4c9f52db35e08a5965dd1021d6
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-02 21:45:17 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Hello"),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.grey,
+      ),
     );
   }
 }
